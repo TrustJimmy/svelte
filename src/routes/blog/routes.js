99abe23ab1cde6da8f6a0e579e95ac52d7e99299
Nikita <r1ck54nchez@yandex.ru>
2024-03-06 20:52:02 +0300

    add blog

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,7 @@
+export const routes = [
+    {
+        id: 1,
+        title: 'My first blog post',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium rem dolor velit rerum quae amet omnis tenetur voluptatem magni?'
+    },
+];
