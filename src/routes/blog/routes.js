4af128722d12d3d6b460aa2e6e2aeb75cbd6b6de
Nikita <r1ck54nchez@yandex.ru>
2024-03-06 21:05:49 +0300

    add post 2

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -4,4 +4,9 @@ export const routes = [
         title: 'My first blog post',
         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium rem dolor velit rerum quae amet omnis tenetur voluptatem magni?'
     },
+    {
+        id: 2,
+        title: 'My second blog post',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium rem dolor velit rerum quae amet omnis tenetur voluptatem magni?'
+    },
 ];
